be0cfe99ec54acc77f37d1c44693d596f84489ca
DwiPratiwi2002 <128199835+DwiPratiwi2002@users.noreply.github.com>
2024-03-18 12:19:37 +0800

    Add files via upload

---
+++ Latihan/Latihan_Dwik/src/latihan_dwik/Dwik.java
@@ -0,0 +1,15 @@
+package latihan_dwik;
+
+/**
+ *
+ * @author Dwik Java
+ */
+public class Dwik {
+    public void cetak(){
+        System.out.println("Data Dwik");
+    }
+    
+public void Tampil(){
+        System.out.println("Cetak Text Dengan Method Tampil");
+    }
+}
---
+++ Latihan/Latihan_Dwik/src/latihan_dwik/Latihan_Dwik.java
@@ -0,0 +1,20 @@
+package latihan_dwik;
+
+/**
+ *
+ * @author Ni Kadek Dwi Pratiwi
+ */
+public class Latihan_Dwik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Latihan Mandiri By Dwik");
+        System.out.println("Ni Kadek Dwi Pratiwi");
+        
+     Dwik D = new Dwik();
+     D.cetak();
+     D.Tampil();      
+    } 
+}
